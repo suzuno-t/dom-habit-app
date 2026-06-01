9ea7b9d45f0ec98844273c69b461369cf22f8b48
suzuno-t <noriko.suzuki.t@gmail.com>
2026-06-01 16:59:30 +0900

    初心に帰る

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-// 画面が閉じられても、裏でじっと時間を数えるお留守番コード
+// 裏で時間を数えて、スマホの画面に通知を強制ポップアップさせるコード
 self.addEventListener("message", function (event) {
   if (event.data && event.data.delay) {
-    // 画面が消えていても、裏で10秒カウントする
     setTimeout(() => {
       const batouMessages = [
         "おい、10秒経ったぞ！いつまでサボってんだコラ！",
@@ -9,16 +8,13 @@ self.addEventListener("message", function (event) {
         "はいサボったー。一生そのままダラダラ過ごせば？",
       ];
       const randomIndex = Math.floor(Math.random() * batouMessages.length);
-      const message = batouMessages[randomIndex];
 
-      // 時間になったら、眠っている表の画面を無理やり叩き起こしてメッセージを送りつける！
-      self.clients.matchAll().then((clients) => {
-        clients.forEach((client) => {
-          client.postMessage({
-            action: "voice-batou",
-            message: message,
-          });
-        });
+      // スマホに通知をドカンと表示する！
+      self.registration.showNotification("【警告】サボり検知", {
+        body: batouMessages[randomIndex],
+        tag: "sabori-alert",
+        renotify: true,
+        requireInteraction: true, // ユーザーが触るまで消えないモード
       });
     }, event.data.delay);
   }
