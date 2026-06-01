a7458aa393b31387f7127730157ec4c4bc867894
suzuno-t <noriko.suzuki.t@gmail.com>
2026-06-01 16:24:50 +0900

    音声版：画面閉じる対応

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
-// スマホが画面を消して眠っても、時間になったら叩き起こして通知を出すお助けコード
+// 画面が閉じられても、裏でじっと時間を数えるお留守番コード
 self.addEventListener("message", function (event) {
   if (event.data && event.data.delay) {
+    // 画面が消えていても、裏で10秒カウントする
     setTimeout(() => {
-      self.registration.showNotification("【警告】サボり検知", {
-        body: "おい、10秒経ったぞ！いつまでサボってんだコラ！",
-        tag: "sabori-alert",
-        renotify: true,
+      const batouMessages = [
+        "おい、10秒経ったぞ！いつまでサボってんだコラ！",
+        "やる気あんの？あんたの習慣化なんてその程度なんだね。",
+        "はいサボったー。一生そのままダラダラ過ごせば？",
+      ];
+      const randomIndex = Math.floor(Math.random() * batouMessages.length);
+      const message = batouMessages[randomIndex];
+
+      // 時間になったら、眠っている表の画面を無理やり叩き起こしてメッセージを送りつける！
+      self.clients.matchAll().then((clients) => {
+        clients.forEach((client) => {
+          client.postMessage({
+            action: "voice-batou",
+            message: message,
+          });
+        });
       });
     }, event.data.delay);
   }
